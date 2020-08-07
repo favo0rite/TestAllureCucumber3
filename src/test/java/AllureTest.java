329bb4cb0c6ceb1a9b1e45bcd8f7a84041c0e2bc
Pavel Zamotaev <pavladar1999@mail.ru>
2020-08-07 19:31:53 +0300

    Исправили pom5

--- src/test/java/AllureTest.java
+++ src/test/java/AllureTest.java
@@ -2,6 +2,7 @@ import managers.ManagerPages;
 import org.junit.After;
 import org.junit.Before;
 
+import org.junit.Test;
 import pages.MortgagePage;
 import pages.MortgageReadyHousePage;
 import steps.BaseSteps;
@@ -13,17 +14,19 @@ import java.util.HashMap;
 
 
 public class AllureTest {
-    protected ManagerPages app = ManagerPages.getManagerPages();
-    private StartPageSteps startPageSteps = new StartPageSteps();
-    private MortgagePageSteps mortgagePageSteps = new MortgagePageSteps();
-    private MortgageReadyHousePageSteps mortgageReadyHousePageSteps = new MortgageReadyHousePageSteps();
+
+
+
     @Before
     public void setUp() {
         BaseSteps.setUp();
     }
 
-    @org.junit.Test
+    @Test
     public void testMortgage() {
+        StartPageSteps startPageSteps = new StartPageSteps();
+        MortgagePageSteps mortgagePageSteps = new MortgagePageSteps();
+        MortgageReadyHousePageSteps mortgageReadyHousePageSteps = new MortgageReadyHousePageSteps();
         startPageSteps.moveToMortgage();
 
         mortgagePageSteps.chooseMortgageOnReadyHouse();
@@ -36,17 +39,16 @@ public class AllureTest {
                 }}
         ).clickCheckBoxLifeInsurance().clickCheckBoxyYoungFamily()
                 .checkResults(
-                new HashMap<String, String>() {{
-                    put("Ежемесячный платеж", "16922");
-                    put("Необходимый доход", "21784");
-                    put("Сумма кредита", "2122000");
-                    put("Процентная ставка", "11");
-                }}
-        );
+                        new HashMap<String, String>() {{
+                            put("Ежемесячный платеж", "16922");
+                            put("Необходимый доход", "21784");
+                            put("Сумма кредита", "2122000");
+                            put("Процентная ставка", "11");
+                        }}
+                );
 
 
-
-           }
+    }
 
     @After
     public void tearDown() {
